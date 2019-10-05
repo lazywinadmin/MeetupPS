5acf83bcbc1e09bd4750406e3543ea1e5e940ea3
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-10-04 18:31:30 -0700

    update deploy condition

--- build/build.psdeploy.ps1
+++ build/build.psdeploy.ps1
@@ -5,15 +5,16 @@
     $ENV:BH* are set by the BuildHelpers module
 #>
 if(
-    $env:modulePath #-and
-    #$env:BHBuildSystem -ne 'Unknown' -and
-    #$env:BHBranchName -eq "master" -and
-    #$env:BHCommitMessage -match '!deploy'
+    $env:modulePath -and
+    $env:buildOutputPath -and
+    $env:BHBuildSystem -ne 'Unknown' -and
+    $env:BHBranchName -eq "master" -and
+    $env:BHCommitMessage -match '!deploy'
 )
 {
     Deploy -Name Module {
         By -DeploymentType PSGalleryModule {
-            FromSource -Source (Join-Path $env:buildOutputPath $env:moduleName)
+            FromSource -Source (Join-Path -path $env:buildOutputPath -ChildPath $env:moduleName)
             To -Targets PSGallery
             WithOptions -Options @{
                 ApiKey = $env:PSGalleryKey
@@ -26,6 +27,9 @@ else
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
     "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+    "`t* buildoutputpath (Current: $env:buildOutputPath) `n" +
+    "`t* modulepath (Current: $env:modulePath) `n" +
+    "`t* modulename (Current: $env:modulename) `n" +
     "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
         Write-Host
 }
